1f820a3adea02c76266cb76edf7bd541e2c251d0
juanonsoftware <juanonsoftware@github.com>
2017-08-07 11:53:53 +0700

    V1.4.0 - DefaultEditableEntityDtoMapperFactory only ignores the map on Note, State, IsAvailable

--- GdNet.Layers.AutoMapper/DefaultEditableEntityDtoMapperFactory.cs
+++ GdNet.Layers.AutoMapper/DefaultEditableEntityDtoMapperFactory.cs
@@ -3,6 +3,10 @@ using Mapping.AutoMapper;
 
 namespace GdNet.Layers.AutoMapper
 {
+    /// <summary>
+    /// This base mapper factory will ignore the map on some properties below: Note, State, IsAvailable.
+    /// Those properties should be used for some additional logics.
+    /// </summary>
     public class DefaultEditableEntityDtoMapperFactory<TSource, TDestination> : AutoMapperFactory<TSource, TDestination>
         where TDestination : EditableEntityDto
     {
@@ -13,10 +17,6 @@ namespace GdNet.Layers.AutoMapper
             MappingExpression
                 .ForMember(p => p.Note, a => a.Ignore())
                 .ForMember(p => p.State, a => a.Ignore())
-                .ForMember(p => p.CreatedAt, a => a.Ignore())
-                .ForMember(p => p.CreatedBy, a => a.Ignore())
-                .ForMember(p => p.LastModifiedAt, a => a.Ignore())
-                .ForMember(p => p.LastModifiedBy, a => a.Ignore())
                 .ForMember(p => p.IsAvailable, a => a.Ignore());
         }
     }
