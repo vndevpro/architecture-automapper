b3e959792159f62358bf989707f2e41441dfddd5
juanonsoftware <juanonsoftware@github.com>
2017-10-11 10:01:40 +0700

    V1.5.0 - Change ignoring properties

--- GdNet.Layers.AutoMapper/DefaultEditableEntityDtoMapperFactory.cs
+++ GdNet.Layers.AutoMapper/DefaultEditableEntityDtoMapperFactory.cs
@@ -4,20 +4,21 @@ using Mapping.AutoMapper;
 namespace GdNet.Layers.AutoMapper
 {
     /// <summary>
-    /// This base mapper factory will ignore the map on some properties below: Note, State, IsAvailable.
+    /// This base mapper factory will ignore the map on some properties below: Note.
     /// Those properties should be used for some additional logics.
     /// </summary>
     public class DefaultEditableEntityDtoMapperFactory<TSource, TDestination> : AutoMapperFactory<TSource, TDestination>
         where TDestination : EditableEntityDto
     {
+        /// <summary>
+        /// Inherites creation from base class and configured to ignore some properties.
+        /// </summary>
         protected override void OnMapperCreated()
         {
             base.OnMapperCreated();
 
             MappingExpression
-                .ForMember(p => p.Note, a => a.Ignore())
-                .ForMember(p => p.State, a => a.Ignore())
-                .ForMember(p => p.IsAvailable, a => a.Ignore());
+                .ForMember(p => p.Note, a => a.Ignore());
         }
     }
 }
--- GdNet.Layers.AutoMapper/DefaultEditableEntityTMapperFactory.cs
+++ GdNet.Layers.AutoMapper/DefaultEditableEntityTMapperFactory.cs
@@ -3,11 +3,18 @@ using Mapping.AutoMapper;
 
 namespace GdNet.Layers.AutoMapper
 {
+    /// <summary>
+    /// This base mapper factory will ignore the map on some properties below: Id, CreatedAt, CreatedBy, LastModifiedAt, LastModifiedBy, Note, IsAvailable, StateLastChangedAt.
+    /// Those properties should be used for some additional logics.
+    /// </summary>
     public class DefaultEditableEntityTMapperFactory<TSource, TDestination, TId> : AutoMapperFactory<TSource, TDestination>
         where TSource : class
         where TDestination : class, IEditableEntityT<TId>
         where TId : new()
     {
+        /// <summary>
+        /// Inherites creation from base class and configured to ignore some properties.
+        /// </summary>
         protected override void OnMapperCreated()
         {
             base.OnMapperCreated();
@@ -18,6 +25,7 @@ namespace GdNet.Layers.AutoMapper
                 .ForMember(p => p.CreatedBy, a => a.Ignore())
                 .ForMember(p => p.LastModifiedAt, a => a.Ignore())
                 .ForMember(p => p.LastModifiedBy, a => a.Ignore())
+                .ForMember(p => p.Note, a => a.Ignore())
                 .ForMember(p => p.IsAvailable, a => a.Ignore())
                 .ForMember(p => p.StateLastChangedAt, a => a.Ignore());
         }
